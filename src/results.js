ded4717d20c5523198d4d0b8c595ff5db5a02444
Andrew Mogan <amogan@fnal.gov>
2024-02-15 19:48:14 +0100

    Add to print statement to see if this works

--- src/results.js
+++ src/results.js
@@ -128,7 +128,7 @@ async function addResults(results, title, summary, displayOptions) {
 
 function addSummary(results) {
   gha.summary.addRaw(
-    `Ran ${results.total_tests} tests in ${prettyDuration(results.total_time)}`,
+    `Ran ${results.total_tests} tests in ${prettyDuration(results.total_time)}. Huzzah!`,
     true
   );
 
