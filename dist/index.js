844946958e049ee3448d00d675429602763c0efd
Andrew Mogan <amogan@fnal.gov>
2024-02-15 15:05:49 -0600

    Bring back huzzah

--- dist/index.js
+++ dist/index.js
@@ -7426,7 +7426,7 @@ async function addResults(results, title, summary, displayOptions) {
 
 function addSummary(results) {
   gha.summary.addRaw(
-    `Ran ${results.total_tests} tests in ${prettyDuration(results.total_time)}.`,
+    `Ran ${results.total_tests} tests in ${prettyDuration(results.total_time)}. Huzzah!`,
     true
   );
 
